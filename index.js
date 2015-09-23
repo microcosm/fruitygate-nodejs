8051af8858997dbf7b1fe348900fe94f17e601d1
Andrew McWilliams <andy@jahya.net>
2015-09-23 18:58:27 -0400

    Cleans up console messaging

--- index.js
+++ index.js
@@ -2,36 +2,52 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var allAddresses = ['http://0.0.0.0:3001', 'http://0.0.0.0:3002'];
+var allAddresses = ['http://0.0.0.0:3001', 'http://0.0.0.0:3002', 'http://0.0.0.0:3003'];
 var gateways = {};
 var thisAddress;
 
-/* render chat interface */
+var chatSocket = 'chat message';
+var gatewaySocket = 'gateway message';
+var handshakeIdentifier = 'hello from http://';
+
+/* chat interface */
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 /* route chat messages */
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('a client connected');
 
   socket.on('disconnect', function(){
-    console.log('chat user disconnected');
+    console.log('a client disconnected');
   });
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    pushToGateways(msg);
-    console.log('chat message: ' + msg);
+  socket.on(chatSocket, function(msg){
+    handleChatMessage(msg);
   });
 
-  socket.on('gateway message', function(msg){
-    io.emit('chat message', msg);
-    console.log('gateway message: ' + msg);
+  socket.on(gatewaySocket, function(msg){
+    handleGatewayMessage(msg);
   });
 });
 
-/* listen on port from arg */
+function handleChatMessage(msg) {
+  console.log(chatSocket + ' received: ' + msg);
+  io.emit(chatSocket, msg);
+  pushToGateways(msg);
+}
+
+function handleGatewayMessage(msg) {
+  if(msg.indexOf(handshakeIdentifier) > -1) {
+    console.log('handshake from ' + msg.replace(handshakeIdentifier, ''));
+  } else {
+    console.log(gatewaySocket + ' received: ' + msg);
+    io.emit(chatSocket, msg);
+  }
+}
+
+/* listen */
 process.argv.forEach(function(val, index) {
   if(isPortArg(val)) {
     listenOnPort(parseInt(val));
@@ -49,6 +65,7 @@ function listenOnPort(port) {
   });
 }
 
+/* broadcast */
 function discoverAddress() {
   var address = http.address();
   thisAddress = 'http://' + address['address'] + ':' + address['port'];
@@ -63,11 +80,11 @@ function discoverGateways() {
       console.log('connected to gateway ' + address);
     }
   });
+  pushToGateways('hello from ' + thisAddress);
 }
 
 function pushToGateways(msg) {
   for(var address in gateways) {
-    console.log('pushing to ' + address);
     gateways[address].emit('gateway message', msg);
   }
 }
