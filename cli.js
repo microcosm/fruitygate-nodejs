6e684d13dc42ccbc558ded995a03dfc4157124c6
Andrew McWilliams <andy@jahya.net>
2015-09-27 08:40:08 -0400

    Requires port name arg for usage

--- cli.js
+++ cli.js
@@ -1,35 +1,41 @@
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
-var serialPort = new SerialPort('/dev/cu.usbmodemfa131', {
-  baudrate: 38400,
-  parser: serialport.parsers.readline('\n')
-}, false);
-
 var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
 
-serialPort.open(function(error) {
-  if ( error ) {
-    console.log('Serial failed to open: ' + error);
-  } else {
-    console.log('Serial open');
-    rl.setPrompt('');
-    rl.prompt();
-
-    serialPort.on('data', function(data) {
-      console.log(data);
-    });
-
-    rl.on('line', function(line) {
-      console.log('writing \'' + line + '\' to serial port...');
-      serialPort.write(line + '\r', function(err, results) {
-        serialPort.drain(function() {
-          rl.prompt();
-        });
-      });
-    }).on('close', function() {
-      console.log('Have a great day!');
-      process.exit(0);
-    });
+process.argv.forEach(function(val, index) {
+  if(index == 2) {
+    openSerialPort(val);
   }
 });
+
+function openSerialPort(portName){
+  var serialPort = new SerialPort(portName, {
+    baudrate: 38400,
+    parser: serialport.parsers.readline('\n')
+  }, false);
+
+  serialPort.open(function(error) {
+    if ( error ) {
+      console.log('Serial failed to open: ' + error);
+    } else {
+      console.log('Serial open');
+      rl.setPrompt('');
+      rl.prompt();
+
+      serialPort.on('data', function(data) {
+        console.log(data);
+      });
+
+      rl.on('line', function(line) {
+        console.log('writing \'' + line + '\' to serial port...');
+        serialPort.write(line + '\r', function(err, results) {
+          rl.prompt();
+        });
+      }).on('close', function() {
+        console.log('Have a great day!');
+        process.exit(0);
+      });
+    }
+  });
+}
