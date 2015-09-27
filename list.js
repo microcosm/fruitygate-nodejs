d9dffaf00429c72ef393c717be2fa4978e6d2f4e
Andrew McWilliams <andy@jahya.net>
2015-09-27 08:31:45 -0400

    Makes list print prettier

--- list.js
+++ list.js
@@ -1,9 +1,14 @@
 var SerialPort = require("serialport").SerialPort
 var serialPort = require("serialport");
+var index = 1;
 serialPort.list(function (err, ports) {
   ports.forEach(function(port) {
-    console.log(port.comName);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
+  	console.log(index + '. Port name: ' + port.comName);
+    console.log('   PNP ID: ' + port.pnpId);
+    console.log('   Manufacturer: ' + port.manufacturer);
+    if(ports.length > index) {
+    	console.log('');
+    }
+    index++;
   });
 });
