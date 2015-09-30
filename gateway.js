1bc73730933a3a1b49cf7822da64b63153a41910
Andrew McWilliams <andy@jahya.net>
2015-09-30 17:06:08 -0400

    Accepts serial args when initialising gateway

--- gateway.js
+++ gateway.js
@@ -1,32 +1,49 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
 
 var allAddresses = ['0.0.0.0:3001', '0.0.0.0:3002', '0.0.0.0:3003'];
 var gateways = {};
 var thisAddress;
+var usageMessage = 'Usage: node gateway HTTP_PORT SERIAL_PORT\nTry \'node list\' to get serial port name';
 
 var localSocket = 'local message';
 var gatewaySocket = 'gateway message';
 
 /* startup */
-process.argv.forEach(function(val, index) {
-  if(isPortArg(val)) {
-    listenOnPort(parseInt(val));
-  }
-});
+processArgs();
 
-function listenOnPort(port) {
-  http.listen(port, function(){
-    discoverAddress();
-    discoverGateways();
-  });
+function processArgs(){
+  if(process.argv.length == 3) {
+    openHttpPort(process.argv[2]);
+    discoverSerialPort();
+  } else if(process.argv.length == 4) {
+    openHttpPort(process.argv[2]);
+    openSerialPort(process.argv[3]);
+  } else {
+    exitWithMessage(usageMessage);
+  }
+}
+
+/* startup - http */
+function openHttpPort(port) {
+  if(isValidPort(port)) {
+    log('Opening http port ' + port);
+    http.listen(port, function(){
+      discoverAddress();
+      discoverGateways();
+    });
+  } else {
+    exitWithMessage(usageMessage);
+  }
 }
 
 function discoverAddress() {
   var address = http.address();
   thisAddress = address['address'] + ':' + address['port'];
-  log('serving clients on [' + thisAddress + ']');
+  log('Serving clients on [' + thisAddress + ']');
 }
 
 function discoverGateways() {
@@ -34,9 +51,34 @@ function discoverGateways() {
     if(address != thisAddress) {
       gateways[address] = require("socket.io-client");
       gateways[address] = gateways[address].connect('http://' + address, { query: 'clientAddress=' + thisAddress });
-      log('listening to gateway [' + address + ']');
+      log('Listening to gateway [' + address + ']');
     }
   });
+}
+
+/* startup - serial */
+function discoverSerialPort() {
+  var found = 0;
+  var name, port;
+  serialport.list(function (err, ports) {
+    for(var i = 0; i < ports.length; i++) {
+      port = ports[i];
+      if(port.manufacturer == 'SEGGER') {
+        found++;
+        name = port.comName;
+      }
+    }
+
+    if(found == 1) {
+      openSerialPort(name);
+    } else {
+      exitWithMessage(usageMessage);
+    }
+  });
+}
+
+function openSerialPort(port){
+  log('Opening serial port ' + port);
 }
 
 /* serve html */
@@ -46,10 +88,10 @@ app.get('/', function(req, res){
 
 /* message handling - websocket I/O */
 io.on('connection', function(socket){
-  log('client [' + clientAddress(socket) + '] just connected');
+  log('Client [' + clientAddress(socket) + '] just connected');
 
   socket.on('disconnect', function(){
-    log('client [' + clientAddress(socket) + '] just disconnected');
+    log('Client [' + clientAddress(socket) + '] just disconnected');
   });
 
   socket.on(localSocket, function(msg){
@@ -66,13 +108,13 @@ io.on('connection', function(socket){
 
 /* message handling - generic */
 function incomingFromLocal(msg, socket) {
-  log('client [' + clientAddress(socket) + '] just sent [' + msg + '], emitting to clients & gateways...');
+  log('Client [' + clientAddress(socket) + '] just sent [' + msg + '], emitting to clients & gateways...');
   io.emit(localSocket, msg);
   pushToGateways(msg);
 }
 
 function incomingFromGateway(msg, socket) {
-  log('gateway [' + clientAddress(socket) + '] just sent [' + msg + '], emitting to clients...');
+  log('Gateway [' + clientAddress(socket) + '] just sent [' + msg + '], emitting to clients...');
   io.emit(localSocket, msg);
 }
 
@@ -83,7 +125,7 @@ function pushToGateways(msg) {
 }
 
 /* utilities */
-function isPortArg(val) {
+function isValidPort(val) {
   return !isNaN(val);
 }
 
@@ -94,4 +136,9 @@ function log(msg) {
 
 function clientAddress(socket) {
   return socket.handshake.query.clientAddress;
+}
+
+function exitWithMessage(str) {
+  console.log(str);
+  process.exit(0);
 }
